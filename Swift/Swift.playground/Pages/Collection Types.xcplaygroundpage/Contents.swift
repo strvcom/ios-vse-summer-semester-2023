e93541a74c058ee59bc5b650ebd8efff1e8d6dab
Jan Schwarz <jan.schwarz@strv.com>
2023-02-23 13:33:13 +0100

    Commit progress from the second lesson

--- Swift/Swift.playground/Pages/Collection Types.xcplaygroundpage/Contents.swift
+++ Swift/Swift.playground/Pages/Collection Types.xcplaygroundpage/Contents.swift
@@ -4,50 +4,87 @@ import Foundation
 //: ## Arrays
 //: ### Declaration
 example(of: "Array declaration") {
-    // [...]()
-    // _: [...]
-    // Array of attendees
-    // Access first element
-    // Set first element
+    var attendees: [String] = ["Tim", "Steve"]
+    let _ = []
+    
+    print(attendees[0])
+    attendees[0] = "Cook"
 }
 //: ### Properties and methods
 example(of: "Array properties and methods") {
-    // Array of attendees
-    // Count
-    // isEmpty
-    // first element
-    // Append element
-    // Append collection
-    // Insert
-    // Remove
+    var attendees: [String] = ["Tim", "Steve"]
+    
+    print(attendees.count)
+    
+    print(attendees.count == 0)
+    print(attendees.isEmpty)
+    
+    // print(attendees[2])
+    print(attendees.first.debugDescription)
+    if let item = attendees.first {
+        print(item)
+    }
+    
+    attendees.append("Craig")
+    print(attendees)
+    
+    attendees.append(contentsOf: ["Jiri", "Vojtech"])
+    // attendees = attendees + ["Jiri", "Vojtech"]
+    print(attendees)
+    
+    attendees.insert("Ondrej", at: 0)
+    print(attendees)
+    
+    attendees.remove(at: 0)
+    print(attendees)
 }
 //: ### Loop array
 example(of: "Loop array") {
-    // Array of attendees
-    // Loop attendees
-    // Loop index to count-1
-    // Loop index to ..< count
-    // Enumerate
+    var attendees: [String] = ["Tim", "Steve"]
+    
+    for attendee in attendees {
+        print(attendee)
+    }
+    
+    for index in 0...(attendees.count-1) {
+        print(attendees[index])
+    }
+    
+    for index in 0..<attendees.count {
+        print(attendees[index])
+    }
+    
+    for (index, attendee) in attendees.enumerated() {
+        print(index, attendee)
+    }
 }
 
 //: ## Sets
 //: ### Declaration
 example(of: "Set declaration") {
-    // Duplicated attendees
-    // Create set
+    var attendees: [String] = ["Tim", "Steve", "Tim"]
+    let set = Set(attendees)
+    print(set)
 }
 //: ## Dictionaries
 //: ### Declaration
 example(of: "Dictionary declaration") {
-    // _ = [...]()
-    // _: [...]
-    // Dictionary with names and amount of them
-    // access
-    // change
+    var attendees: [String: String] = ["Cook": "Tim", "Jobs": "Steve"]
+    let _ = [:]
+    
+    print(attendees["Cook"].debugDescription)
+    print(attendees["Bla"] ?? "Nothing")
+    
+    attendees["Cook"] = "Timothy"
+    attendees["Bla"] = "Huh"
+    print(attendees)
 }
 //: ### Loop
 example(of: "Loop dictionary") {
-    // Dictionary with names of attendees
-    // Loop
+    let attendees: [String: String] = ["Cook": "Tim", "Jobs": "Steve"]
+    
+    for (key, value) in attendees {
+        print(key, value)
+    }
 }
 //: [Next](@next)
